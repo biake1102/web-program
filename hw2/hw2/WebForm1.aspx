5f4ec0b6048ccdf9ffa1f95caa21355edf5e3303
biake1102 <tonyhung931102@gmail.com>
2023-10-13 21:24:29 +0800

    10/13

---
+++ hw2/hw2/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="hw2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <asp:Panel ID="Panel1" runat="server" GroupingText="主餐">
+            &nbsp;漢堡數量:
+            <asp:TextBox ID="txtHamQuan" runat="server">0</asp:TextBox>
+            &nbsp; &nbsp;三明治數量:
+            <asp:TextBox ID="txtSanQuan" runat="server">0</asp:TextBox>
+        </asp:Panel>
+        <asp:Panel ID="Panel2" runat="server" GroupingText="飲料">
+            &nbsp; 豆漿數量:
+            <asp:TextBox ID="txtSoyQuan" runat="server">0</asp:TextBox>
+            &nbsp;
+            <asp:CheckBox ID="chkSoyPea" runat="server" Text="加珍珠" />
+            &nbsp;&nbsp; 奶茶數量:<asp:TextBox ID="txtMilQuan" runat="server">0</asp:TextBox>
+            &nbsp;
+            <asp:CheckBox ID="chkMilPea" runat="server" Text="加珍珠" />
+        </asp:Panel>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="計算價錢" />
+        <br />
+        <br />
+        <asp:Label ID="lblOutput" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ hw2/hw2/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hw2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtHamQuan.Text);
+            int b = Convert.ToInt32(txtSanQuan.Text);
+            int c = Convert.ToInt32(txtSoyQuan.Text);
+            int d = Convert.ToInt32(txtMilQuan.Text);
+            int f = 0 , g = 0 , h = 0, i = 0 , count = a + b + c + d;
+            
+            if (chkSoyPea.Checked)
+            {
+                f = 50 * a + 40 * b + 30 * c + 30 * d;
+                lblOutput.Text = "總價:" + f + "<br/>";
+                lblOutput.Text += "項目數:" + count;
+                if (chkMilPea.Checked)
+                {
+                    g = 50 * a + 40 * b + 25 * c + 35 * d;
+                    lblOutput.Text = "總價:" + g + "<br/>";
+                    lblOutput.Text += "項目數:" + count;
+                    if (chkSoyPea.Checked)
+                    {
+                        h = 50 * a + 40 * b + 30 * c + 35 * d;
+                        lblOutput.Text = "總價:" + h + "<br/>";
+                        lblOutput.Text += "項目數:" + count;
+                    }
+                }
+            }
+            else
+            {
+                i = 50 * a + 40 * b + 25 * c + 30 * d;
+                lblOutput.Text = "總價:" + i + "<br/>";
+                lblOutput.Text += "項目數:" + count;
+            }
+        }
+    }
+}
